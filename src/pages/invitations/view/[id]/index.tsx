c666b36174be80c328c423f29304a842fca74373
roq-ai <>
2023-07-20 06:08:59 +0000

    Initial commit

---
+++ src/pages/invitations/view/[id]/index.tsx
@@ -0,0 +1,124 @@
+import { Box, Center, Flex, Link, List, ListItem, Spinner, Stack, Text } from '@chakra-ui/react';
+import Breadcrumbs from 'components/breadcrumb';
+import { Error } from 'components/error';
+import { FormListItem } from 'components/form-list-item';
+import { FormWrapper } from 'components/form-wrapper';
+import AppLayout from 'layout/app-layout';
+import NextLink from 'next/link';
+import { useRouter } from 'next/router';
+import { FunctionComponent, useState } from 'react';
+import { routes } from 'routes';
+import useSWR from 'swr';
+import { compose } from 'lib/compose';
+import {
+  AccessOperationEnum,
+  AccessServiceEnum,
+  requireNextAuth,
+  useAuthorizationApi,
+  withAuthorization,
+} from '@roq/nextjs';
+import { UserPageTable } from 'components/user-page-table';
+
+import { getInvitationById } from 'apiSdk/invitations';
+import { InvitationInterface } from 'interfaces/invitation';
+
+function InvitationViewPage() {
+  const { hasAccess } = useAuthorizationApi();
+  const router = useRouter();
+  const id = router.query.id as string;
+  const { data, error, isLoading, mutate } = useSWR<InvitationInterface>(
+    () => (id ? `/invitations/${id}` : null),
+    () =>
+      getInvitationById(id, {
+        relations: ['organization', 'user'],
+      }),
+  );
+
+  const [deleteError, setDeleteError] = useState(null);
+  const [createError, setCreateError] = useState(null);
+
+  return (
+    <AppLayout
+      breadcrumbs={
+        <Breadcrumbs
+          items={[
+            {
+              label: 'Invitations',
+              link: '/invitations',
+            },
+            {
+              label: 'Invitation Details',
+              isCurrent: true,
+            },
+          ]}
+        />
+      }
+    >
+      <Box rounded="md">
+        {error && (
+          <Box mb={4}>
+            <Error error={error} />
+          </Box>
+        )}
+        {isLoading ? (
+          <Center>
+            <Spinner />
+          </Center>
+        ) : (
+          <>
+            <FormWrapper>
+              <Stack direction="column" spacing={2} mb={4}>
+                <Text
+                  sx={{
+                    fontSize: '1.875rem',
+                    fontWeight: 700,
+                    color: 'base.content',
+                  }}
+                >
+                  Invitation Details
+                </Text>
+              </Stack>
+              <List spacing={3} w="100%">
+                <FormListItem label="Created At:" text={data?.created_at as unknown as string} />
+
+                <FormListItem label="Updated At:" text={data?.updated_at as unknown as string} />
+
+                {hasAccess('organization', AccessOperationEnum.READ, AccessServiceEnum.PROJECT) && (
+                  <FormListItem
+                    label="Organization:"
+                    text={
+                      <Link as={NextLink} href={`/organizations/view/${data?.organization?.id}`}>
+                        {data?.organization?.name}
+                      </Link>
+                    }
+                  />
+                )}
+                {hasAccess('user', AccessOperationEnum.READ, AccessServiceEnum.PROJECT) && (
+                  <FormListItem
+                    label="User:"
+                    text={
+                      <Link as={NextLink} href={`/users/view/${data?.user?.id}`}>
+                        {data?.user?.email}
+                      </Link>
+                    }
+                  />
+                )}
+              </List>
+            </FormWrapper>
+          </>
+        )}
+      </Box>
+    </AppLayout>
+  );
+}
+
+export default compose(
+  requireNextAuth({
+    redirectTo: '/',
+  }),
+  withAuthorization({
+    service: AccessServiceEnum.PROJECT,
+    entity: 'invitation',
+    operation: AccessOperationEnum.READ,
+  }),
+)(InvitationViewPage);
